6886a34f15d83a402d07f168a08cd9d951963af8
venchak <venchak.bohdan@gmail.com>
2025-06-27 09:55:59 -0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,12 @@
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = os.getenv("ADMIN_ID")
+
 def main():
     print("Hello from Telegram bot project")
 
